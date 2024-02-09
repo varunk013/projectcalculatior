98e7b1b3db12e1d2d705ae72aa0106446c6799bd
Varun <01fe22bca013@kletech.ac.in>
2024-02-09 23:46:11 +0530

    Arthmetic_Opt

---
+++ Math_Arthmetic.java
@@ -0,0 +1,20 @@
+public class Math_Arthmetic {
+    double squre(float opt1){
+        return Math.sqrt(opt1);
+    }
+    double Max(float opt1,float opt2){
+        return Math.max(opt1,opt2);
+    }
+    double Min(float opt1,float opt2){
+        return Math.min(opt1,opt2);
+    }
+    double power(float opt1,float opt2){
+        return Math.pow(opt1,opt2);
+    }
+    double Round(float opt1){
+        return Math.round(opt1);
+    }
+    double Absolute(float opt1){
+        return Math.abs(opt1);
+    }
+}
