5a44d1239e463bdf45d03c4ecc2a44039a8eb768
Varun <01fe22bca013@kletech.ac.in>
2024-02-09 13:22:53 +0530

    Arthemetic Opt

---
+++ MathFunctions.java
@@ -0,0 +1,15 @@
+public class MathFunctions {
+
+    double squre(float num1 ){
+        return num1*num1;
+    }
+    double cube(float num1){
+        return num1*num1*num1;
+    }
+    double module(float num1,float num2){
+        return num1 % num2;
+    }
+    double power(float num1,float num2){
+        return Math.pow(num1, num2);
+    }
+}
